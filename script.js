6d41ae45a99fd0bb93353b28bfe1eb81d561af4c
MAYTINH-8JCGDJ3\MyPC <luutien18195@gmail.com>
2018-08-28 12:55:23 +0700

    fix some bug

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let random_num = 0;
 let heightBrowser = window.innerHeight;
 let widthBrowser = window.innerWidth;
 bnt_y.onclick = function(){
-	alert("!!!");
+	alert("oh! you are right");
 };
 bnt_n.onclick = function(){
 	alert("This button doesn't exist, please choose again!! ");
 };
 bnt_n.onmouseover = function(){
-	bnt_n.style.top = Math.random()*500 + "px";
-	bnt_n.style.left = Math.random()*500 + "px";
+	bnt_n.style.top = Math.random()*heightBrowser + "px";
+	bnt_n.style.left = Math.random()*widthBrowser + "px";
 };
