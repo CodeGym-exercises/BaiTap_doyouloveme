4800cc92fb14b55ec96429aff5eb2ad1c9077bb8
MAYTINH-8JCGDJ3\MyPC <luutien18195@gmail.com>
2018-08-28 12:28:11 +0700

    first commit
    add doyoulove app

---
+++ script.js
@@ -0,0 +1,15 @@
+let bnt_y = document.getElementById("yes");
+let bnt_n = document.getElementById("no");
+let random_num = 0;
+let heightBrowser = window.innerHeight;
+let widthBrowser = window.innerWidth;
+bnt_y.onclick = function(){
+	alert("!!!");
+};
+bnt_n.onclick = function(){
+	alert("This button doesn't exist, please choose again!! ");
+};
+bnt_n.onmouseover = function(){
+	bnt_n.style.top = Math.random()*500 + "px";
+	bnt_n.style.left = Math.random()*500 + "px";
+};
